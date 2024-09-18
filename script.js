bfdce4009bea04f1a0b2b9db52b69946e26c8517
rileyjson <142921318+rileyjson@users.noreply.github.com>
2024-09-18 18:43:50 -0400

    Added a function to set chosen operation

--- script.js
+++ script.js
@@ -2,39 +2,27 @@ let firstNumber, op, secondNumber;
 let buttons = document.querySelectorAll("button");
 let input = document.querySelector(".input-box");
 let equals = document.querySelector(".equals");
-let newFirstNumber;
+let newFirstNum;
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.textContent == equals.textContent) {
-      operate(newFirstNumber, op, secondNumber);
+      operate(newFirstNum, op, secondNumber);
     } else {
       input.value += button.textContent;
       secondNumber = input.value;
 
       switch (button.textContent) {
         case "+":
-          op = "+";
-          firstNumber = input.value;
-          newFirstNumber = firstNumber.replace("+", "");
-          input.value = "";
+          newFirstNum = setOp("+");
           break;
         case "-":
-          op = "-";
-          firstNumber = input.value;
-          newFirstNumber = firstNumber.replace("-", "");
-          input.value = "";
+          newFirstNum = setOp("-");
           break;
         case "*":
-          op = "*";
-          firstNumber = input.value;
-          newFirstNumber = firstNumber.replace("*", "");
-          input.value = "";
+          newFirstNum = setOp("*");
           break;
         case "/":
-          op = "/";
-          firstNumber = input.value;
-          newFirstNumber = firstNumber.replace("/", "");
-          input.value = "";
+          newFirstNum = setOp("/");
           break;
         case "C":
           input.value = "";
@@ -43,6 +31,15 @@ buttons.forEach((button) => {
     }
   });
 });
+
+function setOp(operator) {
+  op = operator;
+  firstNumber = input.value;
+  firstNumberWithoutOperator = firstNumber.replace(operator, "");
+  input.value = "";
+
+  return firstNumberWithoutOperator;
+}
 
 function operate(firstNumber, op, secondNumber) {
   if (op == undefined) {
@@ -54,18 +51,12 @@ function operate(firstNumber, op, secondNumber) {
   switch (op) {
     case "+":
       result = parseInt(firstNumber) + parseInt(secondNumber);
-      input.value = result;
-      firstNumber = result; //lets you chain operations with result
       break;
     case "-":
       result = parseInt(firstNumber) - parseInt(secondNumber);
-      input.value = result;
-      firstNumber = result;
       break;
     case "*":
       result = parseInt(firstNumber) * parseInt(secondNumber);
-      input.value = result;
-      firstNumber = result;
       break;
     case "/":
       if (secondNumber == "0") {
@@ -73,8 +64,9 @@ function operate(firstNumber, op, secondNumber) {
         throw new Error("Divide by 0 error");
       }
       result = parseInt(firstNumber) / parseInt(secondNumber);
-      input.value = result;
-      firstNumber = result;
       break;
   }
+  input.value = result;
+  firstNumber = result; //to chain operations
+  secondNumber = "";
 }
